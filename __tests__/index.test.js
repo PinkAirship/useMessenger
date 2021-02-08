31dbee2f845793b2b329347bb75a3f4652186296
Marc Phillips <marc@crowdstorage.com>
2021-02-08 15:26:52 -0700

    Add eslint for tests and make fixes

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
 import { Messages } from '../example/App'
 import { MessagesProvider } from '../src/contexts/messages-provider'
@@ -18,22 +19,22 @@ it('renders an Add Message Button', async () => {
 })
 
 it('adds a message when clicking the Add Message Button', async () => {
-  const { findByText, queryAllByText } = renderComponent(<Messages />)
-  const removeMessages = await queryAllByText(/Remove Message/)
-  expect(removeMessages).toHaveLength(0)
+  const { findByText, queryByText } = renderComponent(<Messages />)
+  const removeMessages = queryByText(/Remove Message/)
+  expect(removeMessages).not.toBeInTheDocument()
   const button = await findByText(/Add Message/)
   button.click()
   await findByText(/Remove Message/)
 })
 
 it('removes a message when clicking the Remove Message Button', async () => {
-  const { findByText, queryAllByText } = renderComponent(<Messages />)
+  const { findByText, queryByText } = renderComponent(<Messages />)
   let button = await findByText(/Add Message/)
   button.click()
   button = await findByText(/Remove Message/)
   button.click()
-  const removeMessages = await queryAllByText(/Remove Message/)
-  expect(removeMessages).toHaveLength(0)
+  const removeMessages = queryByText(/Remove Message/)
+  expect(removeMessages).not.toBeInTheDocument()
 })
 
 it('alerts screenReader on Add Message', async () => {
